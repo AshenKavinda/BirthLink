272ef31c54cd0e29aff445a21dab13876b4b00bc
Ashen Kavinda <156800673+AshenKavinda@users.noreply.github.com>
2024-06-30 12:13:31 +0530

    Right panel image reset

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -18,10 +18,9 @@ function lordImage() {
                 <span style="font-size: 1.5vw;">Little Miracles, Big Smiles</span>
               </p>
             </div>
-            <div class="h-100 imgContainer">
-              <img src="img/mother.png" alt="Mother Image">
-            </div>
-          </div>`;
+            <div class="h-100 w-100 d-flex flex-column align-items-center justify-content-center imgContainer">
+              <img src="img/coverImg.png" alt="Mother Image">
+            </div>`;
 
       document.getElementById("rightPanel").className = "rightPanels2";
       
@@ -58,24 +57,24 @@ function SignUp() {
 function resetmidPanel() {
     document.getElementById('midPanel').className = "midPanel";
     document.getElementById('midPanel').innerHTML = `
-        <div class="p-5 d-flex flex-column justify-content-center w-100 h-100" style="font-size: 1.2vw;">
+        <div class="p-5 d-flex flex-column justify-content-center w-100 h-100 fontSizeSignIn">
             <label for="" class="hedder mb-4 text-center w-100" style="color: #7E30E1;font-size: 4vw; font-weight: 700;">Welcome !</label>
             <form class="" action="index1.php" method="post">
               <div class="row mb-3">
                 <label for="inputEmail3" class="col-form-label">Email</label>
                 <div class="">
-                <input name="username" type="text" class="form-control form-control-lg" id="inputEmail3" placeholder="Email">
+                <input name="username" type="text" class="form-control form-control-lg" id="inputEmail3" placeholder="Email" required>
                 </div>
               </div>
               <div class="row mb-3">
                 <label for="inputPassword3" class="col-form-label">Password</label>
                 <div class="">
-                  <input name="password" type="password" class="form-control form-control-lg" id="inputPassword3" placeholder="Password" >
-                  <a href="http://" class="float-end text-decoration-none" style="color: #7E30E1; font-size: 0.9vw;">fogot password?</a>
-                  <a href="http://" class="float-start text-decoration-none" style="color: #e13030; font-size: 0.9vw;"><?= $invalid ;?></a>
+                  <input name="password" type="password" class="form-control form-control-lg" id="inputPassword3" placeholder="Password" required>
+                  <a href="http://" class="float-end text-decoration-none fontSizeSignInFogot" style="color: #7E30E1;">fogot password?</a>
+                  <a href="http://" class="float-start text-decoration-none fontSizeSignIn" style="color: #e13030;"><?= $invalid ;?></a>
                 </div>
               </div>
-              <button name="SigninSubmit" type="submit" class="btn w-100 py-2" style="background-color: #7E30E1; font-size: 1.2vw; font-weight: 700;color: #fff;">Sign in</button>
+              <button name="SigninSubmit" type="submit" class="btn w-100 py-2 fontSizeSignIn" style="background-color: #7E30E1;font-weight: 700;color: #fff;">Sign in</button>
             </form>
     
           </div>
