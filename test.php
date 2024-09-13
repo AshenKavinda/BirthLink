4541625d4d94e87bcc2de5624441ff4170409f9a
Ashen Kavinda <156800673+AshenKavinda@users.noreply.github.com>
2024-09-13 18:26:33 +0530

    complete

--- test.php
+++ test.php
@@ -1,15 +1,30 @@
 <?php
- require_once 'models/Mother.php';
- $user = new Mother();
+//  require_once 'models/Mother.php';
+//  $user = new Mother();
 
 
-$result = $user->addUser("Ashen","kavinda","31331","ja;kdnf","jha;ksjdf","jkahkdsj","jsdkjf","11-04-2000");
-//echo $result;
-if ($result) {
-    echo "ok";
-}else {
-    echo "no";
-}
+// $result = $user->addUser("Ashen","kavinda","31331","ja;kdnf","jha;ksjdf","jkahkdsj","jsdkjf","11-04-2000");
+// //echo $result;
+// if ($result) {
+//     echo "ok";
+// }else {
+//     echo "no";
+// }
+
+ require_once 'models/Pregnancy.php';
+ $preg = new PregnancyNote();
+
+ try {
+    $result = $preg->displayByPID(1);
+
+    while($row = mysqli_fetch_assoc($result)){
+       echo $row['name'] . "<br>";
+    }
+ } catch (\Throwable $th) {
+    echo $th;
+ }
+
+
 
 
 ?>
